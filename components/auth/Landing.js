e3c29aa768d933668b3db4bd1fd577856e9b0aef
Arjun Doel <arjundoel@gmail.com>
2021-08-22 13:47:09 +0100

    auth setup

---
+++ components/auth/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View, Button } from 'react-native'
+
+export default function Landing({ navigation }) {
+  return (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Button 
+        title="register"
+        onPress={() => navigation.navigate("Register")}/>
+      <Button 
+        title="login"
+        onPress={() => navigation.navigate("Login")}/>
+    </View>
+  )
+}
